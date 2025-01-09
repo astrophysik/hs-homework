02e8501b23b85ac632a2d977bc19bce5a15c4141
Vadim Sadokhov <v.sadokhov@vk.team>
2025-01-09 16:49:21 +0300

    solve part5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,33 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl f e [] = e
+myFoldl f e (h : t) = myFoldl f (f e h) t 
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr f e [] = e
+myFoldr f e (h : t) = f h (myFoldr f e t)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldl (\curr e -> curr ++ [f e]) []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldl (\curr e -> curr ++ (f e)) [] 
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldl (\curr e -> curr ++ e) []
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldl (\curr e -> (e : curr)) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldl (\curr e -> if (p e) then curr ++ [e] else curr) []
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition p = myFoldl (\curr e -> if (p e)
+                                     then (fst curr ++ [e], snd curr)
+                                     else (fst curr, snd curr ++ [e])) ([], [])
 
